7cd682618453f80d73008ca364690ce58dc375b6
luca.lusnig@mpi-dortmund.mpg.de <luca.lusnig@mpi-dortmund.mpg.de>
2019-12-09 15:20:45 +0100

    added 'FilamentEnhancer->MaskCreator_.java class'

--- stripper/helper.py
+++ stripper/helper.py
@@ -1,6 +1,14 @@
 """
 JAVA CLASS THAT I CONVERTED IN PYTHON DICTIONARY
 """
+from numpy import zeros,ndenumerate,subtract,add
+from math import exp,pi
+# in invert floatProcess img i have to use min and max of float or double?? see in the code
+# https://docs.oracle.com/javase/8/docs/api/constant-values.html#java.lang.Float.MAX_VALUE
+JAVA_MAX_FLOAT32=3.4028234663852886E38
+JAVA_MIN_FLOAT32=1.1754943508222875E-38
+JAVA_MAX_DOUBLE= 1.7976931348623157E308
+JAVA_MIN_DOUBLE= 2.2250738585072014E-308
 
 def createSliceRange(slice_from,slice_to):
     """
@@ -27,3 +35,68 @@ def createFilamentEnhancerContext(	filament_width, mask_width,angle_step,equaliz
               f"\n The default value, False, will be used")
 
     return {"filament_width": int(filament_width), "mask_width": int(mask_width), "angle_step": int(angle_step), "equalize": equalize}
+
+
+def invert(img,m=JAVA_MIN_FLOAT32,M=JAVA_MAX_FLOAT32):
+    """
+    This function simulate the FloatProcessor.invert function. For more info see:
+                        ij.process.FloatProcesor.invert() ln(493)
+    Each pixel in the image is inverted using p=max-(p-min), where 'min' and 'max' are the display range limits
+    :param img:
+    :param m: min value
+    :param M: max value
+    :return: inverted image as in java
+    """
+    subtract(M, subtract(img, m), out=img)
+    return img
+
+def createMask(mask_size, filamentwidth, maskwidth, t):
+    """
+    It is used to return a maskinstead of the FilamentEnhancer->MaskCreator_.java class
+    :param mask_size:
+    :param filamentwidth:
+    :param maskwidth:
+    :param t: type
+    :return:
+    """
+    mask = zeros((mask_size, mask_size), dtype=float)
+    if t not in [0,1]:
+        return mask
+
+    x0 = mask_size/2 + 0.5
+    y0 = mask_size / 2 + 0.5
+    sigmax = maskwidth / 2.355 # Full width at half maximum
+    varx = sigmax * sigmax
+    sigmay = filamentwidth / 2.355
+    vary = sigmay * sigmay
+
+    if t ==1:
+        for index, value in ndenumerate(mask):
+            y=index[1]+0.5
+            x=index[0]+0.5
+            y_y0Power=(y - y0)*(y - y0)
+            mask[index]= -1.0*pi*sigmax*(vary- y_y0Power)/(2*vary*sigmay) * exp( -1.0*( ((x - x0)*(x - x0))/(2*varx) + y_y0Power/(2*vary)  ))
+    elif t == 1:
+        for index, value in ndenumerate(mask):
+            mask[index]= (index[0]-x0)*exp( -1.0 * ( ((index[0]-x0)+(index[0]-x0))/(2*varx) + ((index[1]-y0)+(index[1]-y0))/(2*vary) ) )
+
+    value_to_add= -mask[0,0]
+    add(mask,value_to_add,out=mask)
+
+
+    """
+    the 'fp' is the 'mask' var in the java code
+    The following code is useless because it comments the multiply step
+    // Normalize
+		double sum = 0;
+		for(int x = 0; x < fp.getWidth(); x++){
+			for(int y = 0; y < fp.getHeight(); y++){
+				sum += fp.getf(x, y);
+			}
+		}
+		double scale = 1.0/sum;
+		//fp.multiply(scale);
+    """
+
+    return mask
+
--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,5 +1,7 @@
 import stripper.helper as helper
 import unittest
+from numpy import array,array_equal,allclose,zeros
+from copy import deepcopy
 
 class Test_javaClass_in_pythonDict(unittest.TestCase):
     def test_createSliceRange(self):
@@ -13,3 +15,30 @@ class Test_javaClass_in_pythonDict(unittest.TestCase):
         self.assertEqual(FilamentEnhancerContext["mask_width"], 5)
         self.assertEqual(FilamentEnhancerContext["angle_step"], 3)
         self.assertTrue(FilamentEnhancerContext["equalize"])
+
+
+
+class Test_invert(unittest.TestCase):
+    def test_invert(self):
+        x = array([[1, 2.0], [0, 0], [2, 3.]])
+        expected_array = array([[-0.7 ,-1.7], [0.3, 0.3], [-1.7, -2.7]])
+        self.assertTrue(allclose(helper.invert(img=deepcopy(x),m=0.1,M=0.2),expected_array, atol=0.0000001))
+
+
+#for now only test_invalid_type it is running
+class Test_createMask(unittest.TestCase):
+    img= array([[1, 2.0], [0, 0]])
+    mask_size=2
+
+    def test_invalid_type(self):
+        self.assertTrue(array_equal(zeros((self.mask_size, self.mask_size), dtype=float),helper.createMask(mask_size=self.mask_size, filamentwidth=0.3, maskwidth=2, t=5)))
+
+    def test_type0(self):
+        expected_array= array([[1, 2.0], [0, 0]])
+        out_array=helper.createMask(mask_size=self.mask_size, filamentwidth=0.3, maskwidth=2, t=0)
+        self.assertTrue(allclose(out_array, expected_array, atol=0.0000001))
+
+    def test_type1(self):
+        expected_array= array([[1, 2.0], [0, 0]])
+        out_array=helper.createMask(mask_size=self.mask_size, filamentwidth=0.3, maskwidth=2, t=1)
+        self.assertTrue(allclose(out_array, expected_array, atol=0.0000001))
