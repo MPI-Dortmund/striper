0032094dbd58bca559dbc9f964013d6a7b852761
luca.lusnig@mpi-dortmund.mpg.de <luca.lusnig@mpi-dortmund.mpg.de>
2019-12-09 13:08:27 +0100

    added 'FilamentEnhancer->FilamentEnhancerContext.java class'

--- stripper/helper.py
+++ stripper/helper.py
@@ -1,3 +1,7 @@
+"""
+JAVA CLASS THAT I CONVERTED IN PYTHON DICTIONARY
+"""
+
 def createSliceRange(slice_from,slice_to):
     """
     It is used to create a dict instead of the helicalPicker->gui->SliceRange.java class
@@ -6,3 +10,20 @@ def createSliceRange(slice_from,slice_to):
     :return: dict
     """
     return {"slice_from":int(slice_from),"slice_to":int(slice_to)}
+
+
+def createFilamentEnhancerContext(	filament_width, mask_width,angle_step,equalize = False):
+    """
+    It is used to create a dict instead of the FilamentEnhancer->FilamentEnhancerContext.java class
+    :param filament_width: The filament width defines the how broad a filament is (measured in pixel)
+    :param mask_width: mask width
+    :param angle_step: The angle step defines the degree how fine the directions will be enhanced (measured in degree)
+    :param equalize: If equalization is turned on, large high contrast contamination don't have less effect on
+                     the detection. For default is False
+    :return: dict
+    """
+    if isinstance(equalize,bool) is False:
+        print(f"WARNING: invalid 'FilamentEnhancerContext.equalize' values: {equalize}. It has to be a boolean value."
+              f"\n The default value, False, will be used")
+
+    return {"filament_width": int(filament_width), "mask_width": int(mask_width), "angle_step": int(angle_step), "equalize": equalize}
--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,8 +1,15 @@
 import stripper.helper as helper
 import unittest
 
-class Test_createSliceRange(unittest.TestCase):
+class Test_javaClass_in_pythonDict(unittest.TestCase):
     def test_createSliceRange(self):
         sliceRange=helper.createSliceRange(slice_from=0,slice_to=5)
         self.assertEqual(sliceRange["slice_from"],0)
         self.assertEqual(sliceRange["slice_to"], 5)
+
+    def test_createFilamentEnhancerContext(self):
+        FilamentEnhancerContext=helper.createFilamentEnhancerContext(	filament_width=0, mask_width=5,angle_step=3,equalize = True)
+        self.assertEqual(FilamentEnhancerContext["filament_width"],0)
+        self.assertEqual(FilamentEnhancerContext["mask_width"], 5)
+        self.assertEqual(FilamentEnhancerContext["angle_step"], 3)
+        self.assertTrue(FilamentEnhancerContext["equalize"])
