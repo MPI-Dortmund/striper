6c492e03ae69126b53b7d2b20ed6a5c56bb5855d
luca.lusnig@mpi-dortmund.mpg.de <luca.lusnig@mpi-dortmund.mpg.de>
2019-12-09 11:02:02 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,61 @@
+"""
+MIT License
+
+Copyright (c) 2019 Max Planck Institute of Molecular Physiology
+
+Author: Luca Lusnig (luca.lusnig@mpi-dortmund.mpg.de)
+Author: Thorsten Wagner (thorsten.wagner@mpi-dortmund.mpg.de)
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+from setuptools import setup
+import os
+import re
+import codecs
+# Create new package with python setup.py sdist
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+setup(
+    name='stripper',
+    version=find_version("stripper", "__init__.py"),
+    python_requires='>3.7.0',
+    license='MIT',
+    install_requires=[
+        "numpy==1.18.0rc1",
+        "pillow==6.2.1",
+        "mrcfile==1.1.2"
+    ],
+    description='STRIPPER: faST Robust fIlament Picking ProcEduRe',
+)
---
+++ stripper/main.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def run():
+    print("START:",datetime.now())
+    print("END:", datetime.now())
+
+if __name__ == "__main__":
+    run()
