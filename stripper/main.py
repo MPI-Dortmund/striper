29adffcb1fc91ae150405fa9768da14dd7034d00
luca.lusnig@mpi-dortmund.mpg.de <luca.lusnig@mpi-dortmund.mpg.de>
2019-12-10 09:35:37 +0100

    removed useless class FHT.py

--- stripper/main.py
+++ stripper/main.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from  stripper.helper import getTransformedMasks
 
 def run():
+    a= getTransformedMasks(32, 1, 1, 4, 0)
     print("START:",datetime.now())
     print("END:", datetime.now())
 
