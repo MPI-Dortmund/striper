ad11ed7a6d73b3afcda4c1e64ca4cb1c3c035e95
luca.lusnig@mpi-dortmund.mpg.de <luca.lusnig@mpi-dortmund.mpg.de>
2019-12-12 15:01:46 +0100

    added params parser

--- stripper/main.py
+++ stripper/main.py
@@ -1,9 +1,27 @@
 from datetime import datetime
-from  stripper.helper import getTransformedMasks
-
+from argparse import ArgumentParser
+from stripper.params import Params
+from PIL import Image
+from stripper.helper import normalizeImg,INTEGER_8BIT_MIN,INTEGER_8BIT_MAX,resize_img
+import mrcfile
+from numpy import asarray
 def run():
-    a= getTransformedMasks(32, 1, 1, 4, 0)
+    parser = ArgumentParser("stripper parser tool")
+    parser.add_argument(dest="config_filename",type=str, nargs='?',help="name of the config_file to use. Default value is 'example_config.json'")
+    args=parser.parse_args()
+    config_filename = args.config_filename if args.config_filename is not None else "example_config.json"
     print("START:",datetime.now())
+    params=Params(config_filename)
+    try:
+        img=mrcfile.open(params.config_path_to_file).data
+    except ValueError:
+        img=asarray(Image.open(params.config_path_to_file))
+
+    if params.resize is not None:
+        img=resize_img(img=img,resize=params.resize)
+    if params.convert8bit is True:
+        img=normalizeImg(img=img,new_max=INTEGER_8BIT_MAX,new_min=INTEGER_8BIT_MIN)
+
     print("END:", datetime.now())
 
 if __name__ == "__main__":
---
+++ stripper/params.py
@@ -0,0 +1,65 @@
+
+from json import load as json_load
+from json import JSONDecodeError
+from os import path
+from PIL import Image
+from stripper.helper import createSliceRange
+from stripper.filamentDetector import createFilamentDetectorContext,filamentWidthToSigma
+from stripper.filamentEnhancer import createFilamentEnhancerContext
+from stripper.filamentFilter import createFilamentFilterContext
+from math import sqrt
+
+
+
+def load_json(config_path):
+    """
+    Check if the config file exist and has a valid json format
+    :param config_path: path to the json config file
+    :return: loaded json file
+    """
+    if path.isfile(config_path) is False:
+        print("ERROR: file '"+config_path+"' not found!")
+        exit(-1)
+    try:
+        with open(config_path) as json_file:
+            return  json_load(json_file)
+    except JSONDecodeError:
+        print("Your configuration file seems to be corruped. Please check if it is valid.")
+
+class Params:
+    def __init__(self, cfg):
+        self.cfg=load_json(cfg)
+        self.config_path_to_file = self.cfg["path_to_file"]
+        self.slice_range=createSliceRange(slice_from=0,slice_to=0)          #todo we will change these values when I'll open the stack, if it is a stack
+
+        self.detectorContext=createFilamentDetectorContext(sigma=filamentWidthToSigma(self.cfg["Detection"]["Filament_width"]),
+                                                           lower_threshold=self.cfg["Detection"]["Lower_Threshold"],
+                                                           upper_threshold=self.cfg["Detection"]["Upper_Threshold"])
+
+        self.enhancerContext=createFilamentEnhancerContext(filament_width=self.cfg["Detection"]["Filament_width"],
+                                                           mask_width=self.cfg["Detection"]["Mask_width"],
+                                                           angle_step=2,
+                                                           equalize=self.cfg["Detection"]["Equalize"])
+
+        m=self.cfg["Filtering"]["Custom_mask"]
+        min_filament_distance= sqrt( 2*(self.cfg["Filtering"]["Allowed_box_overlapping"]*self.cfg["General"]["Box_size"])*(self.cfg["Filtering"]["Allowed_box_overlapping"]*self.cfg["General"]["Box_size"])  ) /2
+
+        self.filterContext=createFilamentFilterContext(min_number_boxes = self.cfg["Filtering"]["Min_number_of_boxes"],
+                                    min_line_straightness = self.cfg["Filtering"]["Min_straightness"],
+                                    window_width_straightness = self.cfg["Filtering"]["Window_size"],
+                                    removement_radius = int(self.cfg["General"]["Box_size"]/2),
+                                    fit_distribution = False,
+                                    sigma_min_response = self.cfg["Filtering"]["Sigma_min_response"],
+                                    sigma_max_response = self.cfg["Filtering"]["Sigma_max_response"],
+                                    min_filament_distance = min_filament_distance,
+                                    border_diameter = int(self.cfg["General"]["Box_size"]/2),
+                                    double_filament_insensitivity = 1-self.cfg["Filtering"]["Sensitivity"],
+                                    userFilters = None,
+                                    box_size = self.cfg["General"]["Box_size"],
+                                    box_distance = self.cfg["General"]["Box_distance"],
+                                    mask = None if m is False else Image.open(m))
+        self.convert8bit=self.cfg["General"]["Convert_to_8bit"] if "Convert_to_8bit" in self.cfg["General"] else False
+        self.resize = (self.cfg["General"]["Resize"],self.cfg["General"]["Resize"]) if "Resize" in self.cfg["General"] else None
+
+
+
